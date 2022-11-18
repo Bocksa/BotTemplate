f44df5bc87091f3685afb75254aa7af7ad415575
Bocks <88006808+Bocksa@users.noreply.github.com>
2022-11-18 18:25:03 +0000

    Added Demo Command

---
+++ Commands/CommandModule.cs
@@ -0,0 +1,10 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace ConsoleApp1.Commands {
+    public class SlashCommands : ApplicationCommandModule {
+        [SlashCommand("Demo-Command", "This is a demo command.")]
+        public async Task demo_command(InteractionContext ctx) {
+            await ctx.CreateResponseAsync("Hello " + ctx.Member.Nickname + ". This is a demo command");
+        }
+    }
+}
