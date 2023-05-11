f578143af10eeebb68ee14c201d9eba5305e881b
Bocks <cian@telecomeireann.ie>
2023-05-11 20:54:04 +0100

    template funnies

--- Commands/CommandModule.cs
+++ Commands/CommandModule.cs
@@ -6,5 +6,9 @@ namespace ConsoleApp1.Commands {
         public async Task demo_command(InteractionContext ctx) {
             await ctx.CreateResponseAsync("Hello " + ctx.Member.Nickname + ". This is a demo command");
         }
+        [SlashCommand("Repeat", "Repeat an inputted string.")]
+        public async Task repeat(InteractionContext ctx, [Option("string", "The string you want to have repeated")] string inputString) {
+            await ctx.CreateResponseAsync(inputString);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,46 @@
-﻿// Documentation:
-// https://dsharpplus.github.io/articles/basics/bot_account.html
-// https://dsharpplus.github.io/articles/basics/first_bot.html
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.SlashCommands;
 
-namespace ConsoleApp1 {
-    internal class Program {
-        static void Main(string[] args) {
-            MainAsync().GetAwaiter().GetResult();
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
         }
 
-        static async Task MainAsync() {
-            
+        public static DiscordClient Discord { get; private set; }
+
+        public Task Status(DiscordClient c, HeartbeatEventArgs e)
+        {
+            var x = new DiscordActivity(name: "", type: ActivityType.Custom);
+            Discord.UpdateStatusAsync(x);
+            return Task.CompletedTask;
+        }
+        public async Task MainAsync()
+        {
+
+            Discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "",
+                TokenType = TokenType.Bot,
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+                LogTimestampFormat = "MMM dd yyyy - hh:mm:ss tt",
+                Intents = DiscordIntents.All
+            });
+
+            Discord.Heartbeated += Status;
+
+            var commandsConfig = new SlashCommandsConfiguration { }; // Currently blank idk if i will ever need it
+            var slash = Discord.UseSlashCommands(commandsConfig);
+
+            slash.RegisterCommands<ConsoleApp1.Commands.SlashCommands>(); // Registers all commands in ConsoleApp1 > Commands > SlashCommands
+
+            await Discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
